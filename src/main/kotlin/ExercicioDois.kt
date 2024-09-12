2b362dd69934a6d263f9ed6b885de5d9dbbb37ef
Wellington <wellington@wellimovel.com>
2024-09-12 11:11:32 -0300

    Inserção do exercicio 2 e da lista de exercicios

---
+++ src/main/kotlin/ExercicioDois.kt
@@ -0,0 +1,10 @@
+//Faça um Programa que peça um número e então mostre a mensagem O número
+//informado foi [número].
+package org.example
+
+fun ExercicioDois(){
+    println("Informe qualquer numero")
+    val numero = readLine()
+    println("Você digitou o numero $numero")
+
+}
