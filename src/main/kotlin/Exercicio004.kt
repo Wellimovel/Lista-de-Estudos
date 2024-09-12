36b17ff675d6bb373d528907f8ca981dbcebfa1d
Wellington <wellington@wellimovel.com>
2024-09-12 12:35:15 -0300

    inserção do exercicio04

--- src/main/kotlin/Exercicio004.kt
+++ src/main/kotlin/Exercicio004.kt
@@ -1,4 +1,23 @@
+//#### Exercicio 004
+//
+//Faça um Programa que peça as 4 notas bimestrais e mostre a média.
 package org.example
 
-class Exercicio004 {
+
+fun main(){
+    fun Exercicio004() : Double{
+        val notas = mutableListOf<Double>()
+        for(i in 1..4){
+            print("Digite a nota $i: ")
+            val nota = readLine()?.toDoubleOrNull()?:continue
+            notas.add(nota)
+        }
+        val soma = notas.sum()
+        val media = soma / notas.size
+        return media
+
+    }
+    val media = Exercicio004()
+    println(" Media: $media")
+
 }
