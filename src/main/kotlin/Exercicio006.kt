876076ed7665ef2157ba5dd4e776c4e193fbbefc
Wellington <wellington@wellimovel.com>
2024-09-13 12:43:09 -0300

    insersão do exercicio 006

---
+++ src/main/kotlin/Exercicio006.kt
@@ -0,0 +1,20 @@
+package org.example
+
+import kotlin.math.PI
+import kotlin.math.pow
+
+fun main(){
+    fun Exercicio006(): Double {
+        var raio: Double =0.0
+        println("Por gentileza preencha o tamanho do raio em cm")
+        raio = readln().toDouble()
+        var diametro:Double = raio.pow(2.0)
+        var area:Double = diametro.times(3.14)
+        var areaDoRaio:Double = area
+        return areaDoRaio
+
+    }
+    val areaDoRaio :Double=Exercicio006()
+    println("$areaDoRaio")
+
+}
